9f2dc0da6abbda26fa15d3778df8fc754d6e6730
melvinfranco99 <melvinfrancopedraza@gmail.com>
2024-02-20 13:45:51 +0100

    Base de todo proyecto

---
+++ extra-webpack.config.js
@@ -0,0 +1,20 @@
+const webpack = require('webpack');
+
+const config = {
+    resolve: {
+        fallback: {
+            "assert": require.resolve("assert/"),
+            "crypto": require.resolve("crypto-browserify"),
+            "http": require.resolve("stream-http"),
+            "https": require.resolve("https-browserify"),
+            "os": require.resolve("os-browserify/browser"),
+            "stream": require.resolve("stream-browserify"),
+            "url": require.resolve("url"),
+            "zlib": require.resolve("browserify-zlib")
+        }
+    },
+    plugins: [
+        new webpack.ProvidePlugin({ Buffer: ['buffer', 'Buffer']}),
+        new webpack.ProvidePlugin({ process: "process/browser"}),
+    ]
+};
