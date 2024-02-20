600cd28dc4322ad8e5a68927af0f51574e9a3a80
melvinfranco99 <melvinfrancopedraza@gmail.com>
2024-02-20 13:57:47 +0100

    Base del proyecto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public appPages = [
-    { title: 'Login', url: '/login', icon: 'user' },
+    { title: 'Login', url: '/login', icon: 'person' },
     { title: 'Home', url: '/home', icon: 'home' },
   ];
 
